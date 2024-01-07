fc8b9f3fe07ef25c3ba6338a18014245a6cf3c00
Matthew <matthewvat@gmail.com>
2024-01-06 21:47:09 -0500

    add routes

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,28 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config(); 
+require('dotenv').config();
+
+const express = require('express'); 
+const mongoose = require('mongoose'); 
 const uri = process.env.ATLAS_URI; 
+const port = process.env.PORT; 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+mongoose.connect(uri);
+const database = mongoose.connection;
 
-async function run() {
-  try {
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir); 
+database.on('error', (error) => {
+  console.log(error) 
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
+const app = express(); 
+app.use(express.json()); 
+
+const routes = require('./routes/routes');
+app.use('/api', routes);
+
+
 
 
 
